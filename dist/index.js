ce4e1df7ab21c999e4a6fe88f342b63cf74766c5
Joakim Beijar <jbeijar@mios.fi>
2016-07-15 23:38:01 +0300

    Fix ignorefile and ignored files

---
+++ dist/index.js
@@ -0,0 +1,25 @@
+"use strict";
+const gutil = require('gulp-util');
+const through = require('through2');
+const nunjucks = require('nunjucks');
+function default_1(template) {
+    return through.obj(function (file, encoding, callback) {
+        if (file.isNull()) {
+            callback(null, file);
+            return;
+        }
+        if (file.isStream() || !(file.contents instanceof Buffer)) {
+            callback(new gutil.PluginError('gulp-guidestyle', 'Streaming not supported'));
+            return;
+        }
+        var data = JSON.parse(file.contents.toString());
+        var res = nunjucks.render(template, data);
+        var resFile = file.clone({ contents: false });
+        resFile.contents = new Buffer(data);
+        resFile.extname = ".html";
+    });
+}
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.default = default_1;
+;
+//# sourceMappingURL=index.js.map
